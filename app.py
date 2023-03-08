d5bec6f8874785b00fa52437ded2f91445b8c29c
George Michel Machado Pereira <george_pereira@sicredi.com.br>
2023-03-08 17:01:07 -0300

    Criação dos métodos get, post, patch e delete

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from flask_restful import Api
-from resources.hotel import Hotel
+from resources.hotel import Hotel, Hoteis
 
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(Hotel, '/hoteis')
+api.add_resource(Hoteis, '/hotel')
+api.add_resource(Hotel, '/hotel/<int:hotel_id>')
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
--- resources/hotel.py
+++ resources/hotel.py
@@ -1,22 +1,22 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 
 hoteis = [
     {
-        'hote_id': 'alpha',
+        'hotel_id': 1,
         'nome': 'Alpha Hotel',
         'estrelas': 4.3,
         'diaria': 420.34,
         'cidade': 'Rio de Janeiro'
     },
     {
-        'hote_id': 'beta',
+        'hotel_id': 2,
         'nome': 'Beta Hotel',
         'estrelas': 4.5,
         'diaria': 420.34,
         'cidade': 'Belo Horizonte'
     },
     {
-        'hote_id': 'gama',
+        'hotel_id': 3,
         'nome': 'Gama Hotel',
         'estrelas': 4.3,
         'diaria': 420.34,
@@ -24,6 +24,70 @@ hoteis = [
     }
 ]
 
-class Hotel(Resource):
+class Hoteis(Resource):
     def get(self):
         return {'hoteis': hoteis}
+
+class Hotel(Resource):
+    argumentos = reqparse.RequestParser()
+    argumentos.add_argument('nome')
+    argumentos.add_argument('estrelas')
+    argumentos.add_argument('diaria')
+    argumentos.add_argument('cidade')
+        
+    def find_hotel_by_id(self, hotel_id):
+        for hotel in hoteis:
+            if hotel['hotel_id'] == hotel_id:
+                return hotel
+        return None
+            
+    def get(self, hotel_id):
+        hotel = self.find_hotel_by_id(hotel_id)
+        
+        if hotel is None:
+            return {'message': 'Not found'}, 404
+        return hotel   
+        
+    def post(self, hotel_id):
+        hotel = self.find_hotel_by_id(hotel_id)
+        
+        if hotel:
+            return { 'message': 'Hotel id already exists' }, 400
+        
+        dados = Hotel.argumentos.parse_args()
+        
+        novo_hotel = {
+            'hotel_id': hotel_id,
+            **dados
+        }
+        
+        hoteis.append(novo_hotel)
+        return novo_hotel
+    
+    def patch(self, hotel_id):
+        hotel = self.find_hotel_by_id(hotel_id)
+        
+        if hotel is None:
+            return {'message': 'Not found'}, 404
+        
+        dados = Hotel.argumentos.parse_args()
+        
+        novo_hotel = {
+            'hotel_id': hotel_id,
+            **dados
+        }
+        
+        hotel.update(novo_hotel)
+        
+        return hotel
+    
+    def delete(self, hotel_id):
+        hotel = self.find_hotel_by_id(self)
+        
+        if hotel is None:
+            return {'message': 'Not found'}, 404
+        
+        global hoteis
+        hoteis = [hotel for hotel in hoteis if hotel['hotel_id'] is not hotel_id]
+        return {'message': 'Hotel {} was deleted'.format(hotel_id)}, 200
+        
