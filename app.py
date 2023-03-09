3199de0c02f3192ecb6a9b3e764d6c6fe7d2d3d0
George Michel Machado Pereira <george_pereira@sicredi.com.br>
2023-03-09 14:10:31 -0300

    Configuração do sql alchemy

--- app.py
+++ app.py
@@ -3,10 +3,18 @@ from flask_restful import Api
 from resources.hotel import Hotel, Hoteis
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hoteis.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api = Api(app)
+
+@app.before_first_request
+def create_database():
+    database.create_all()
 
 api.add_resource(Hoteis, '/hotel/')
 api.add_resource(Hotel, '/hotel/<int:hotel_id>/')
 
 if __name__ == '__main__':
+    from sql_alchemy import database
+    database.init_app(app)
     app.run(debug=True, port=8080)
--- models/hotel.py
+++ models/hotel.py
@@ -1,4 +1,15 @@
-class HotelModel:
+from sql_alchemy import database
+
+class HotelModel(database.Model):
+    
+    __tablename__ = 'hotel'
+    
+    hotel_id = database.Column(database.Integer, primary_key=True)
+    nome = database.Column(database.String(100))
+    estrelas = database.Column(database.Float(precision=1))
+    diaria = database.Column(database.Float(precision=2))
+    cidade = database.Column(database.String(100))
+    
     def __init__(self, hotel_id, nome, estrelas, diaria, cidade) -> None:
         self.hotel_id = hotel_id
         self.nome = nome
